9adc6a5014845549c505e117351f4328bbe34d75
tharindunirmala488-bit <tharindunirmala488@gmail.com>
2025-08-07 08:51:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " sjsnssjshhshrhrbbb", // ඔයාගෙ session id එක
-MONGODB: process.env.MONGODB || "mongodb://mongo:sjjkkd",  //ඔයාගෙ public mongoDb url එක
+SESSION_ID: process.env.SESSION_ID || "CcUBhR5T#dcy3YT8wu_5MLxZnDFlI0F1a-rXA8Bm6uiaGdgCDXfk", // ඔයාගෙ session id එක
+MONGODB: process.env.MONGODB || "mongodb://mongo:yphYdGclpJQQRIdMdRDsyOfspcuWFVZd@mainline.proxy.rlwy.net:56427",  //ඔයාගෙ public mongoDb url එක
 };
  
